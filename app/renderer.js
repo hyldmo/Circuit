d5882b3d1568bb00069eb8a5bfb2063694aa9c2e
Eivind Hyldmo <eivind.hyldmo@avanade.com>
2017-04-14 14:04:39 +0200

    Moved client bootstrap code to renderer.js

--- app/renderer.js
+++ app/renderer.js
@@ -1,3 +1,10 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const script = document.createElement('script');
+const port = process.env.PORT || 1337;
+script.src = process.env.NODE_ENV == 'development'
+    ? 'http://localhost:' + port + '/dist/bundle.js?mode=' + process.env.NODE_ENV
+    : './dist/bundle.js';
+// TODO: Writing the script path should be done with webpack
+document.body.appendChild(script);
