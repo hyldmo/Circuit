80035631f1c167270776507e6c5383f3b8e26369
Eivind Hyldmo <eivind.hyldmo@avanade.com>
2017-04-16 00:25:16 +0200

    Added more status messages to status channel

--- app/irc/server.ts
+++ app/irc/server.ts
@@ -6,6 +6,18 @@ const options = {
     port: 31130
 }
 
+interface RawMessage {
+    prefix?: string // The prefix for the message (optional)
+    nick?: string // The nickname portion of the prefix (optional)
+    user?: string // The username portion of the prefix (optional)
+    host?: string // The hostname portion of the prefix (optional)
+    server: string // The servername (if the prefix was a servername)
+    rawCommand: string // The command exactly as sent from the server
+    command: string // Human readable version of the command
+    commandType: 'normal'|'error'|'reply'// normal, error, or reply
+    args: string[] // arguments to the command
+}
+
 const wss = new WebSocket.Server(options)
 console.log(`Listening for websocket connections on port ${options.port}`)
 
@@ -18,15 +30,22 @@ wss.on('connection', ws => {
         autoRejoin: true
     })
     console.log(`Connecting to ${server}:${port}`)
-    client.addListener('registered', function (from, to, message) {
-        ws.send(`Connected to ${server}:${port}`)
+    ws.send(`Connecting to ${server}:${port}`)
+
+    client.addListener('registered', function (message: RawMessage) {
+        ws.send(`Connected to ${server}:${port} (${message.command})`)
     })
 
     client.addListener('message', (from, to, message) => {
         ws.send(`${from}=>${to}:${message}`)
     })
 
-    client.addListener('error', message => {
+    client.addListener('raw', (msg: RawMessage) => {
+        if (msg.commandType !== 'reply') return
+        ws.send(msg.args.slice(1).join(' '))
+    })
+
+    client.addListener('error', (message: RawMessage) => {
         ws.send(message.command)
     })
 
